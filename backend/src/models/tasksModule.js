b10d4d0fd368e3b6f5a97a053e461a38968e52c0
André Fernandes <andre@celebrar.com.br>
2024-01-04 14:06:24 -0300

    wip: update getAll function to get tasks from mysql

--- backend/src/models/tasksModule.js
+++ backend/src/models/tasksModule.js
@@ -1,9 +1,18 @@
+//import mysql connection
+const connection = require('./connection');
+
 /**
  * @name getAll
  * @description get all tasks data from mySql
+ * @constant tasks to save recived tasks from mysql data
  *
  */
-const getAll = () => {};
+const getAll = async() => {
+  const tasks = await connection.execute('SELECT * FROM tasks'); // using connection.execute to execute sql querys
+  
+  //return tasks data
+  return tasks;
+};
 
 module.exports = {
   getAll,
