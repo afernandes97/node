0c0d544d588f53e45ec83f850b62617aa9003bc8
André Fernandes <andre@celebrar.com.br>
2024-01-04 14:00:10 -0300

    wip: creating .env to get data from mysql connection

--- backend/src/models/connection.js
+++ backend/src/models/connection.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql2/promise');
 
-// creating a list of connections using creatPool
+// creating a list of connections using creatPool and configure pool using .env data
 const connection = mysql.createPool({
-  host: '',
-  user: '',
-  password: '',
-  database: '',
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
 });
 
 // export connection
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,10 @@
 //import app
 const app = require('./app');
+//import dotenv to access .env archive
+require('dotenv').config();
+
+//recive port from .env, if this port don't exist, use 3333
+const PORT = process.env.PORT || 3333;
 
 //up server on port 3333
-app.listen(3333, () => console.log('Server is running'));
+app.listen(PORT, () => console.log('Server is running'));
